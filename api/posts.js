694c8695111e3641ac747d547da1c004b9ece622
Cherrie <cherrie.alfonso@gmail.com>
2020-05-24 21:30:52 -0400

    Finished week 2, day 3

--- api/posts.js
+++ api/posts.js
@@ -1,7 +1,41 @@
 const express = require('express');
 const postsRouter = express.Router();
 
-const {getAllPosts} = require("../db");
+const {getAllPosts, createPost, updatePost, getPostById} = require("../db");
+
+const { requireUser } = require('./utils');
+
+postsRouter.post('/', requireUser, async (req, res, next) => {
+  const {id} = req.user;
+  console.log(req.body)
+  const { title, content, tags = "" } = req.body;
+  console.log(title)
+  const tagArr = tags.trim().split(/\s+/)
+  const postData = {};
+
+  if (tagArr.length > 0) {
+    postData.tags = tagArr;
+  }
+
+  try {
+    // add authorId, title, content to postData object
+    postData.authorId = id;
+    postData.title = title;
+    postData.content = content;
+    const post = await createPost(postData);
+    // if the post comes back, res.send({ post });
+    if (post) {
+      res.send({post});
+    } else {
+      next({
+        name: 'unableToCreatePost', 
+        message: 'Unable to create post'
+      })
+    }
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
 
 postsRouter.use((req, res, next) => {
   console.log("A request is being made to /posts");
@@ -15,6 +49,68 @@ postsRouter.get('/', async (req, res) => {
   res.send({
     posts
   });
+});
+
+
+postsRouter.patch('/:postId', requireUser, async (req, res, next) => {
+  const { postId } = req.params;
+  const { title, content, tags } = req.body;
+
+  const updateFields = {};
+
+  if (tags && tags.length > 0) {
+    updateFields.tags = tags.trim().split(/\s+/);
+  }
+
+  if (title) {
+    updateFields.title = title;
+  }
+
+  if (content) {
+    updateFields.content = content;
+  }
+
+  try {
+    const originalPost = await getPostById(postId);
+
+    console.log("LOGGING THIS: " + originalPost.author.id, req.user.id)
+
+    if (originalPost.author.id === req.user.id) {
+      const updatedPost = await updatePost(postId, updateFields);
+      res.send({ post: updatedPost })
+    } else {
+      next({
+        name: 'UnauthorizedUserError',
+        message: 'You cannot update a post that is not yours'
+      })
+    }
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
+postsRouter.delete('/:postId', requireUser, async (req, res, next) => {
+  try {
+    const post = await getPostById(req.params.postId);
+
+    if (post && post.author.id === req.user.id) {
+      const updatedPost = await updatePost(post.id, { active: false });
+
+      res.send({ post: updatedPost });
+    } else {
+      // if there was a post, throw UnauthorizedUserError, otherwise throw PostNotFoundError
+      next(post ? { 
+        name: "UnauthorizedUserError",
+        message: "You cannot delete a post which is not yours"
+      } : {
+        name: "PostNotFoundError",
+        message: "That post does not exist"
+      });
+    }
+
+  } catch ({ name, message }) {
+    next({ name, message })
+  }
 });
 
 module.exports = postsRouter;
--- notepad.md
+++ notepad.md
@@ -13,3 +13,10 @@ VALUES
 
 TAGS COMING OUT AS "[ARRAY]"
 AUTHOR COMING OUT AS "[OBJECT]"
+
+
+curl http://localhost:3000/api/posts -X POST -H 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwidXNlcm5hbWUiOiJyb24iLCJpYXQiOjE1OTAzMTAwODV9.JkrWLKDs7lxxfzU0FbMqTkv1q1QrMoAjRTTzHdKiwtM' -H 'Content-Type: application/json' -d '{"title": "test post", "content": "how is this?", "tags": " #once #twice    #happy"}'
+
+curl http://localhost:3000/api/posts/3 -X PATCH -H 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwidXNlcm5hbWUiOiJyb24iLCJpYXQiOjE1OTAzMTAwODV9.JkrWLKDs7lxxfzU0FbMqTkv1q1QrMoAjRTTzHdKiwtM' -H 'Content-Type: application/json' -d '{"title": "updating my old stuff", "tags": "#oldisnewagain"}'
+
+curl http://localhost:3000/api/posts -H 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwidXNlcm5hbWUiOiJyb24iLCJpYXQiOjE1OTAzMTAwODV9.JkrWLKDs7lxxfzU0FbMqTkv1q1QrMoAjRTTzHdKiwtM'
